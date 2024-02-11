2d3d6f339af8c161e01c754c3bceb44c33ff5c98
Andres <andresfe1396@gmail.com>
2024-02-11 12:08:01 -0500

    path alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,13 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
+  experimental: {
+    clientPrerender: true,
+  },
   site: "https://example.com",
   integrations: [preact()]
 });
